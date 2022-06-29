10bc4e4bf0019496ad9e288c967429e89f99e8c0
ABHINAVMANGAL <abhinavmangal68@gmail.com>
2022-06-29 17:01:37 +0530

    a

--- src/controller/internsController.js
+++ src/controller/internsController.js
@@ -5,49 +5,50 @@ const  createIntern= async function(req,res){
     try{
     const data= req.body
     if(Object.keys(data).length===0){
-        res.status(400).send({status:false,msg:"body couldnot empty"})
+        return res.status(400).send({status:false,msg:"body couldnot empty"})
     }
     if(!data.name){
-        res.status(400).send({status:false,msg:"name is required"})
+        return res.status(400).send({status:false,msg:"name is required"})
     }
     if(typeof data.name != "string"){
-        res.status(400).send({status:false,msg:"name is the string require"})
+       return  res.status(400).send({status:false,msg:"name is the string require"})
     }
    let Name=data.name
    let name=Name.trim()
   
 
    if(name!=Name){
-    res.status(400).send({status:false, msg:"space not allowed"})
+    return res.status(400).send({status:false, msg:"space not allowed"})
    }
    if(!data.email){
-    res.status(400).send({status:false,msg:"email is require"})
+    return  res.status(400).send({status:false,msg:"email is require"})
    }
  
 
    const email=data.email
    if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))){
-    res.status(400).send({status:false,msg:"enter valid email id"})
+   return  res.status(400).send({status:false,msg:"enter valid email id"})
    }
    if(!data. mobile){
-    res.status(400).send({status:false,msg:"mobile number must be present"})
+   return  res.status(400).send({status:false,msg:"mobile number must be present"})
    }
+   
    const mobile=data.mobile
    const mobileNumberAlready= await InterModel.findOne({mobile:mobile})
    if(mobileNumberAlready){
-    res.status(400).send({status:false,msg:"mobile number already register"})
+    return res.status(400).send({status:false,msg:"mobile number already register"})
    }
    
    
   
   const IsemailAlredayused= await InterModel.findOne({email:email});
   if(IsemailAlredayused){
-    res.status(400).send({status:false,msg:"email already registered"})
+   return  res.status(400).send({status:false,msg:"email already registered"})
     
   }
        
      const interndata= await InterModel.create(data)
-     res.status(201).send({status:true,data:interndata})
+    return  res.status(201).send({status:true,data:interndata})
 
 }
    catch(err){
