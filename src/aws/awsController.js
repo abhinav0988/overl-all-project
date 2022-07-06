a6efb9835d193d9f9d84e7422e4e0a6f31b9bd0e
Duttji1996 <96298162+Duttji1996@users.noreply.github.com>
2022-07-06 16:33:30 +0530

    Update awsController.js
    hiding AWS password, and key

--- src/aws/awsController.js
+++ src/aws/awsController.js
@@ -1,9 +1,9 @@
 const aws = require("aws-sdk")
 
 aws.config.update({
-    accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
-    secretAccessKey: "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",
-    region: "ap-south-1"
+    accessKeyId: "******************************",
+    secretAccessKey: "**********************",
+    region: "************"
 })
 
 let uploadFile = async (file) => {
