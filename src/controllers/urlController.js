b9da03896bd5c1b41d67aa40590d715d100ce810
ABHINAVMANGAL <abhinavmangal68@gmail.com>
2022-07-20 17:59:17 +0530

    m

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,6 +1,22 @@
 const urlModel = require("../models/urlModel");
 const shortid = require("shortid");
 const validUrl = require("valid-url");
+const redis = require("redis");
+const redisClient = redis.createClient(
+  13190,
+  "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");}
+);
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 
 const shortenUrl = async function (req, res) {
   try {
@@ -14,6 +30,7 @@ const shortenUrl = async function (req, res) {
     if (!longUrl || typeof longUrl == "undefined" || typeof longUrl == "null" || longUrl.length==0) {
       res.status(400).send({ status: false, message: "Please enter longUrl" });
       return;
+      
     }
     if (!validUrl.isUri(longUrl)) {
       res
@@ -48,6 +65,14 @@ const shortenUrl = async function (req, res) {
 
 const getUrl = async (req, res) => {
   try {
+    let cahcedProfileData = await GET_ASYNC(`${req.params.a}`)
+    if(cahcedProfileData) {
+      res.send(cahcedProfileData)
+    } else {
+      let profile = await authorModel.findById(req.params.authorId);
+      await SET_ASYNC(`${req.params.authorId}`, JSON.stringify(profile))
+      res.send({ data: profile });
+    }
     const url = req.params.urlCode;
     const dbUrl = await urlModel.findOne({ urlCode: url });
     if (dbUrl) {
